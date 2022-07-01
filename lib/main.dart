9e8e35ff7f15c91d5ab201c24ad06b28ae93d03b
redetdereje <redetdereje91@gmail.com>
2022-07-01 17:03:46 -0400

    first commit - finished app

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: const Text('I Am Rich'), // idk what the const does
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: const Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
+        ),
+      ),
+    ),
+  ));
+}
